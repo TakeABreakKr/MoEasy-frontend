c4743e4b82f1f4e569d7ec8d5861618fd56d6e23
ring-wdr <akswnd55@gmail.com>
2024-06-16 08:26:18 +0900

    lint & prettier setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-
-import styles from "./page.module.css";
+import styles from './page.module.css';
 
 export default function Home() {
-  
-  return (
-    <main className={styles.main}>
-        메인페이지입니다.
-    </main>
-  );
+  return <main className={styles.main}>메인페이지입니다.</main>;
 }
