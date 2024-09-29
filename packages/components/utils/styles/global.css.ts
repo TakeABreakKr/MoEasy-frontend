df3ff722cbda5a981a50d3a022fe56e5322ff3a3
ring-wdr <akswnd55@gmail.com>
2024-09-29 22:05:48 +0900

    design: import variables from figma

--- packages/components/utils/styles/global.css.ts
+++ packages/components/utils/styles/global.css.ts
@@ -1,8 +1,77 @@
 // styles/global.css.ts
 import { rem } from '../css';
-import { createVar, globalStyle } from '@vanilla-extract/css';
+import { createGlobalTheme, createVar, globalStyle } from '@vanilla-extract/css';
 
 export const headerHeight = createVar();
+
+const colorVariables = {
+  purple: 'hsl(263, 91%, 62%)',
+  neutral: {
+    black: 'hsl(0, 0%, 0%)',
+    95: 'hsl(240, 3%, 10%)',
+    80: 'hsl(240, 2%, 20%)',
+    70: 'hsl(210, 1%, 30%)',
+    60: 'hsl(210, 1%, 40%)',
+    50: 'hsl(210, 1%, 50%)',
+    40: 'hsl(210, 2%, 60%)',
+    30: 'hsl(210, 3%, 70%)',
+    20: 'hsl(210, 4%, 80%)',
+    10: 'hsl(210, 5%, 90%)',
+    5: 'hsl(210, 10%, 95%)',
+    3: 'hsl(210, 20%, 98%)',
+    white: 'hsl(0, 0%, 100%)',
+  },
+  blue: {
+    99: 'hsl(213, 85%, 4%)',
+    95: 'hsl(213, 93%, 10%)',
+    90: 'hsl(213, 93%, 16%)',
+    80: 'hsl(213, 92%, 25%)',
+    70: 'hsl(213, 91%, 33%)',
+    60: 'hsl(213, 90%, 42%)',
+    50: 'hsl(213, 100%, 56%)',
+    40: 'hsl(213, 100%, 66%)',
+    30: 'hsl(213, 100%, 74%)',
+    20: 'hsl(213, 100%, 83%)',
+    10: 'hsl(213, 100%, 91%)',
+    5: 'hsl(213, 100%, 96%)',
+  },
+  red: {
+    99: 'hsl(0, 59%, 10%)',
+    95: 'hsl(0, 53%, 12%)',
+    90: 'hsl(0, 53%, 16%)',
+    80: 'hsl(0, 55%, 28%)',
+    70: 'hsl(0, 56%, 38%)',
+    60: 'hsl(0, 57%, 48%)',
+    50: 'hsl(0, 100%, 62%)',
+    40: 'hsl(0, 68%, 68%)',
+    30: 'hsl(0, 71%, 76%)',
+    20: 'hsl(0, 69%, 84%)',
+    10: 'hsl(0, 80%, 96%)',
+    5: 'hsl(0, 80%, 98%)',
+  },
+  yellow: {
+    99: 'hsl(42, 97%, 25%)',
+    95: 'hsl(42, 98%, 31%)',
+    90: 'hsl(42, 98%, 36%)',
+    80: 'hsl(42, 96%, 42%)',
+    70: 'hsl(43, 96%, 47%)',
+    60: 'hsl(44, 96%, 51%)',
+    50: 'hsl(47, 100%, 61%)',
+    40: 'hsl(51, 100%, 65%)',
+    30: 'hsl(51, 100%, 75%)',
+    20: 'hsl(51, 100%, 84%)',
+    10: 'hsl(51, 100%, 92%)',
+    5: 'hsl(51, 100%, 96%)',
+  },
+} as const;
+
+export const globalVars = createGlobalTheme(':root', {
+  color: colorVariables,
+  gradient: {
+    neutralWhite: 'linear-gradient(270deg, hsl(0, 0%, 100%) 0%, hsla(0, 0%, 100%, 0) 100%)',
+    yellowBlue: 'linear-gradient(180deg, hsl(213, 100%, 74%) 0%, hsl(47, 100%, 61%) 100%)',
+  },
+});
 
 // box-sizing 규칙을 명시합니다.
 globalStyle('*', {
