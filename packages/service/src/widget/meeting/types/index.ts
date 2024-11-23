6b82c93f67f62a52aa2a775ad64622dda2ec341c
ring-wdr <akswnd55@gmail.com>
2024-11-23 15:53:55 +0900

    feat: type definition change and temporary remove previous component

--- packages/service/src/widget/meeting/types/index.ts
+++ packages/service/src/widget/meeting/types/index.ts
@@ -2,14 +2,14 @@ import { components } from '@/shared/api/my-schema';
 
 import { NameTagProps } from '@moeasy/storybook/ui/tag/nametag/nametag';
 
-export type MeetingType = components['schemas']['MeetingListMeetingDto'];
+export type MeetingType = components['schemas']['MeetingListMeetingDto'] & {
+  members: Member[];
+  limit: number;
+};
 export type MeetingAuthority = MeetingType['authority'];
-export type CardMember = {
+export type Member = {
+  memberId: string;
   name: string;
   userRole?: NameTagProps['userRole'];
-  avatar?: string;
+  thumbnail?: string;
 };
-export type MeetingCardPopupState =
-  | { popupType: 'MEETING'; fromOutside?: boolean }
-  | { popupType: 'MEMBER'; fromOutside?: boolean; member: CardMember }
-  | null;
