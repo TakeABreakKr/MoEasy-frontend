348a465f31b006c9792c717260c0bc38011be4c1
ring-wdr <akswnd55@gmail.com>
2024-12-07 01:57:52 +0900

    refactor: split step component

--- ui/create/step-button/index.tsx
+++ ui/create/step-button/index.tsx
@@ -1,77 +1,47 @@
-import { useFormStatus } from 'react-dom';
-
 import Link from 'next/link';
 
 import * as styles from './step-button.css';
 
-type CreateStepButtonProps = {
-  step: number;
-  steps: string[];
-  searchParams: URLSearchParams;
-  /** step에 마지막에 표시되는 글자 */
-  finishMessage?: string;
-  /** form 전송 중 표시되는 글자 */
-  messageOnPending?: string;
-  pathname: string;
+export type CreateButtonCommonProps = {
+  prevText?: string;
+  nextText?: string;
+  onPrevStep: () => void;
+  onNextStep: () => void;
 };
 
-export function CreateStepButton({
-  step,
-  steps,
-  searchParams,
-  finishMessage,
-  messageOnPending = 'loading...',
-  pathname,
-}: CreateStepButtonProps) {
-  const searchParamsObject = Object.fromEntries(searchParams);
+export function CreateButtonFirst({
+  prevText = '이전',
+  nextText = '다음',
+  prevHref,
+  onNextStep,
+}: Omit<CreateButtonCommonProps, 'onPrevStep'> & { prevHref: string }) {
   return (
     <div className={styles.navigation}>
-      {step === 1 && (
-        <Link className={styles.navButton} href="/mypage">
-          이전
-        </Link>
-      )}
-      {step !== 1 && (
-        <Link
-          className={styles.navButton}
-          href={{
-            pathname,
-            query: { ...searchParamsObject, step: Number(searchParamsObject.step || '1') - 1 },
-          }}
-        >
-          이전
-        </Link>
-      )}
-      {step !== steps.length && (
-        <Link
-          className={styles.navButton}
-          href={{
-            pathname,
-            query: { ...searchParamsObject, step: Number(searchParamsObject.step || '1') + 1 },
-          }}
-        >
-          다음
-        </Link>
-      )}
-      {step === steps.length && (
-        <FormCreateSubmitButton messageOnPending={messageOnPending}>{finishMessage}</FormCreateSubmitButton>
-      )}
+      <Link className={styles.navButton} href={prevHref}>
+        {prevText}
+      </Link>
+      <button type="button" className={styles.navButton} onClick={onNextStep}>
+        {nextText}
+      </button>
     </div>
   );
 }
 
-function FormCreateSubmitButton({
-  children,
-  messageOnPending = 'loading...',
-}: {
-  children: React.ReactNode;
-  messageOnPending?: string;
-}) {
-  const { pending } = useFormStatus();
+export function CreateButtonCommon({
+  prevText = '이전',
+  nextText = '다음',
+  onPrevStep,
+  onNextStep,
+}: CreateButtonCommonProps) {
   return (
-    <button type="submit" className={styles.navButton} disabled={pending}>
-      {pending ? messageOnPending : children}
-    </button>
+    <div className={styles.navigation}>
+      <button type="button" className={styles.navButton} onClick={onPrevStep}>
+        {prevText}
+      </button>
+      <button type="button" className={styles.navButton} onClick={onNextStep}>
+        {nextText}
+      </button>
+    </div>
   );
 }
 
