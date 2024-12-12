c51c9c51ccae9bfd2979855d62b266b84ffbc215
ring-wdr <akswnd55@gmail.com>
2024-12-12 14:19:37 +0900

    refactor: unused code remove

--- utils/useControlledState.ts
+++
@@ -1,6 +0,0 @@
-import { useState } from 'react';
-
-export const useControlledState = <T>(value?: T, defaultValue?: T) => {
-  const [state, setState] = useState(defaultValue);
-  return [(value ?? state) as T, typeof value === 'undefined' ? setState : undefined] as const;
-};
