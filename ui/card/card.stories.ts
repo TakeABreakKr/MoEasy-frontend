a7bf63f8170555f8626327750346af841d0ad13c
ring-wdr <akswnd55@gmail.com>
2024-12-13 21:54:05 +0900

    refactor: 공통 컴포넌트 기능 수정

--- ui/card/card.stories.ts
+++ ui/card/card.stories.ts
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Card from './card';
-import { CardMember } from './compound-card';
 
 const exampleMembers = [
   { userRole: 'admin', name: '모임장' },
@@ -11,7 +10,7 @@ const exampleMembers = [
   { name: '모임원' },
   { name: '모임원' },
   { name: '모임원' },
-] satisfies CardMember[];
+];
 
 const meta = {
   title: 'Common/Card',
@@ -24,7 +23,6 @@ const meta = {
     idx: 1,
     title: 'Team TAB',
     description: `사이드 프로젝트: 디스코드 봇 모이지(moeasy)를 완성시키는 것을 목표로 하는 모임입니다. 사이드 프로젝트: 디스코드 봇 모이지(moeasy)를 완성시키는 것을 목표로 하는 모..로 하는 모..로 하는 모...`,
-    members: exampleMembers,
   },
 } satisfies Meta<typeof Card>;
 
