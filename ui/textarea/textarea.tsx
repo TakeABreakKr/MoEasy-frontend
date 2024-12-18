5ad6f245a4bd59d187d2e76deeedd78ac4ec5c71
ring-wdr <akswnd55@gmail.com>
2024-12-19 01:38:05 +0900

    refactor: textarea에서 공통으로 사용 가능한 로직 분리

--- ui/textarea/textarea.tsx
+++ ui/textarea/textarea.tsx
@@ -1,103 +1,80 @@
-import React, { ChangeEvent, ComponentPropsWithoutRef, useLayoutEffect, useRef } from 'react';
+import React, { ChangeEvent, forwardRef, useLayoutEffect } from 'react';
 import clsx from 'clsx';
 
+import { useControlledRef } from '../../hooks/use-controlled-ref';
 import { useControlledState } from '../../hooks/use-controlled-state';
 import { Delay } from '../delay';
 import { XIcon } from '../icon';
 import { validateInput } from '../input/input';
 
+import { AutoResizeTextareaProps, resizeTextarea } from '.';
+
 import * as inputStyles from '../input/input.css';
 import { textareaStyles } from './textarea.css';
 
-interface AutoResizeTextareaProps extends ComponentPropsWithoutRef<'textarea'> {
-  value?: string;
-  isError?: boolean;
-  dispatchError?: (isError: boolean) => void;
-  defaultValue?: string | undefined;
-  onValueChange: (input: string) => void;
-  placeholder?: string;
-  minHeight?: number;
-  maxHeight?: number;
-}
+export const Textarea = forwardRef<HTMLTextAreaElement, AutoResizeTextareaProps>(
+  ({ onValueChange, dispatchError, minHeight = 40, maxHeight = 400, isError, className, maxLength, ...props }, ref) => {
+    const [value, setValue] = useControlledState({
+      prop: props.value,
+      defaultProp: props.defaultValue || '',
+      onChange: onValueChange,
+    });
+    const currentLength = value.length;
+    const textareaRef = useControlledRef<HTMLTextAreaElement>(ref);
 
-const resizeTextarea = (ref: HTMLTextAreaElement | null, minHeight = 40, maxHeight = 400) => {
-  if (ref) {
-    ref.rows = 1;
-    ref.style.height = 'auto';
-    const scrollHeight = ref.scrollHeight;
-    ref.style.height = `${Math.max(minHeight, Math.min(scrollHeight, maxHeight))}px`;
-  }
-};
+    useLayoutEffect(() => {
+      resizeTextarea(textareaRef.current, minHeight, maxHeight);
+    }, [textareaRef, minHeight, maxHeight]);
 
-export const Textarea = ({
-  onValueChange,
-  dispatchError,
-  minHeight = 40,
-  maxHeight = 400,
-  isError,
-  className,
-  maxLength,
-  ...props
-}: AutoResizeTextareaProps) => {
-  const [value, setValue] = useControlledState({
-    prop: props.value,
-    defaultProp: props.defaultValue || '',
-    onChange: onValueChange,
-  });
-  const currentLength = value.length;
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
+    const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+      onValueChange?.(event.target.value);
+      setValue?.(event.target.value);
+      resizeTextarea(textareaRef.current, minHeight, maxHeight);
+    };
 
-  useLayoutEffect(() => {
-    resizeTextarea(textareaRef.current);
-  }, []);
+    const refresh = () => {
+      if (textareaRef?.current) {
+        const curr = textareaRef.current;
+        curr.value = '';
+        const validationResult = validateInput(curr);
+        dispatchError?.(validationResult);
+        setValue?.('');
+        onValueChange?.('');
+        resizeTextarea(textareaRef.current, minHeight, maxHeight);
+      }
+    };
 
-  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    onValueChange(event.target.value);
-    setValue?.(event.target.value);
-    resizeTextarea(textareaRef.current);
-  };
-
-  const refresh = () => {
-    if (textareaRef?.current) {
-      const curr = textareaRef.current;
-      curr.value = '';
-      const validationResult = validateInput(curr);
-      dispatchError?.(validationResult);
-      setValue?.('');
-      onValueChange?.('');
-      resizeTextarea(textareaRef.current);
-    }
-  };
-
-  return (
-    <div className={clsx(inputStyles.inputWrapper, className)}>
-      <textarea
-        ref={textareaRef}
-        onChange={handleChange}
-        className={clsx(textareaStyles, inputStyles.inputVariants.classNames.base, className)}
-        style={{ minHeight, maxHeight }}
-        maxLength={maxLength}
-        {...props}
-      />
-      <Delay ms={0}>
-        <span className={inputStyles.inputCtlWrapper}>
-          {currentLength !== 0 && (
-            <button className={inputStyles.resetXIconStyles} onClick={refresh}>
-              <XIcon color="#fff" />
-            </button>
-          )}
-          {maxLength && (
-            <span>
-              <span
-                className={clsx(currentLength === 0 ? inputStyles.ctlTextMax : isError && inputStyles.errorTextColor)}
-              >
-                {currentLength <= maxLength ? currentLength : maxLength}
+    return (
+      <div className={clsx(inputStyles.inputWrapper, className)}>
+        <textarea
+          ref={textareaRef}
+          onChange={handleChange}
+          className={clsx(textareaStyles, inputStyles.inputVariants.classNames.base, className)}
+          style={{ minHeight, maxHeight }}
+          maxLength={maxLength}
+          {...props}
+        />
+        <Delay ms={0}>
+          <span className={inputStyles.inputCtlWrapper}>
+            {currentLength !== 0 && (
+              <button className={inputStyles.resetXIconStyles} onClick={refresh}>
+                <XIcon color="#fff" />
+              </button>
+            )}
+            {maxLength && (
+              <span>
+                <span
+                  className={clsx(currentLength === 0 ? inputStyles.ctlTextMax : isError && inputStyles.errorTextColor)}
+                >
+                  {currentLength <= maxLength ? currentLength : maxLength}
+                </span>
+                <span className={inputStyles.ctlTextMax}>/{maxLength}</span>
               </span>
-              <span className={inputStyles.ctlTextMax}>/{maxLength}</span>
-            </span>
-          )}
-        </span>
-      </Delay>
-    </div>
-  );
-};
+            )}
+          </span>
+        </Delay>
+      </div>
+    );
+  },
+);
+Textarea.displayName = 'Textarea';
