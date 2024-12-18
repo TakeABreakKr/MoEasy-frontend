04657f45527a2ea0d1d8721ae868944fc8accbfa
ring-wdr <akswnd55@gmail.com>
2024-12-19 01:38:05 +0900

    refactor: textarea에서 공통으로 사용 가능한 로직 분리

--- packages/components/ui/textarea/index.ts
+++ packages/components/ui/textarea/index.ts
@@ -1,1 +1,23 @@
+import { ComponentPropsWithoutRef } from 'react';
+
 export { Textarea } from './textarea';
+
+export type AutoResizeTextareaProps = ComponentPropsWithoutRef<'textarea'> & {
+  value?: string;
+  isError?: boolean;
+  dispatchError?: (isError: boolean) => void;
+  defaultValue?: string | undefined;
+  onValueChange?: (input: string) => void;
+  placeholder?: string;
+  minHeight?: number;
+  maxHeight?: number;
+};
+
+export const resizeTextarea = (ref: HTMLTextAreaElement | null, minHeight = 40, maxHeight = 400) => {
+  if (ref) {
+    ref.rows = 1;
+    ref.style.height = 'auto';
+    const scrollHeight = ref.scrollHeight;
+    ref.style.height = `${Math.max(minHeight, Math.min(scrollHeight, maxHeight))}px`;
+  }
+};
