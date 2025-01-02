3cd727e35cdae73d606e53fcf62c3f9c238eacd5
ring-wdr <akswnd55@gmail.com>
2025-01-02 22:29:29 +0900

    fix: change import statement

--- packages/service/src/shared/api/gen-script.ts
+++ packages/service/src/shared/api/gen-script.ts
@@ -1,15 +1,15 @@
-import { factory } from 'typescript';
+import ts from 'typescript';
 import { writeFileSync } from 'fs';
 import openapiTS, { astToString } from 'openapi-typescript';
 
-const FILE = factory.createTypeReferenceNode('File'); // `File`
-const NULL = factory.createLiteralTypeNode(factory.createNull()); // `null`
+const FILE = ts.factory.createTypeReferenceNode('File'); // `File`
+const NULL = ts.factory.createLiteralTypeNode(ts.factory.createNull()); // `null`
 
 const generateTypes = async () => {
   const ast = await openapiTS(new URL('./swagger-spec.json', import.meta.url), {
     transform(schemaObject) {
       if (schemaObject.type === 'string' && schemaObject.format === 'binary') {
-        return schemaObject.nullable ? factory.createUnionTypeNode([FILE, NULL]) : FILE;
+        return schemaObject.nullable ? ts.factory.createUnionTypeNode([FILE, NULL]) : FILE;
       }
     },
   });
