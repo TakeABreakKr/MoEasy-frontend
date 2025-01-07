5778e4825968f038385f10f7deb66c0a45b33905
ring-wdr <akswnd55@gmail.com>
2025-01-07 23:47:49 +0900

    chore: suite 폰트 적용

--- packages/service/src/page/about/index.tsx
+++ packages/service/src/page/about/index.tsx
@@ -1,15 +1,18 @@
 'use client';
 
 import { useRef } from 'react';
+import clsx from 'clsx';
 
 import { AboutToNextPage } from '@/widget/about/to-next-page';
+
+import { suite } from '@moeasy/storybook/font';
 
 import * as styles from './about.css';
 
 export function AboutPage({ children }: { children?: React.ReactNode }) {
   const parentRef = useRef<HTMLElement>(null);
   return (
-    <main ref={parentRef} className={styles.aboutMain}>
+    <main ref={parentRef} className={clsx(styles.aboutMain, suite.className)}>
       {children}
       <AboutToNextPage parentRef={parentRef} />
     </main>
