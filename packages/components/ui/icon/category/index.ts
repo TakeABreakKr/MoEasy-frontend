799949ecc6be1e53164e715b15027f2e51eae901
ring-wdr <akswnd55@gmail.com>
2025-01-18 16:00:35 +0900

    feat: match icons to category variable

--- packages/components/ui/icon/category/index.ts
+++ packages/components/ui/icon/category/index.ts
@@ -1,17 +1,63 @@
-export { default as BeautyIcon } from './beauty';
-export { default as CarIcon } from './car';
-export { default as DrinkIcon } from './drink';
-export { default as EnvironmentIcon } from './environment';
-export { default as FamilyIcon } from './family';
-export { default as FoodIcon } from './food';
-export { default as GameIcon } from './game';
-export { default as HealthIcon } from './health';
-export { default as LoveIcon } from './love';
-export { default as MindIcon } from './mind';
-export { default as OutdoorIcon } from './outdoor';
-export { default as PetIcon } from './pet';
-export { default as PlantIcon } from './plant';
-export { default as ShowIcon } from './show';
-export { default as SocialIcon } from './social';
-export { default as SportsIcon } from './sports';
-export { default as VolunteerIcon } from './volunteer';
+import BeautyIcon from './beauty';
+import BookIcon from './book';
+import CarIcon from './car';
+import CareerIcon from './career';
+import CraftIcon from './craft';
+import CultureIcon from './culture';
+import DanceIcon from './dance';
+import DrinkIcon from './drink';
+import EmploymentIcon from './employment';
+import EnvironmentIcon from './environment';
+import FamilyIcon from './family';
+import FoodIcon from './food';
+import ForeignLanguageIcon from './foreign-language';
+import GameIcon from './game';
+import HealthIcon from './health';
+import InteriorIcon from './interior';
+import LoveIcon from './love';
+import MediaIcon from './media';
+import MindIcon from './mind';
+import MusicIcon from './music';
+import OutdoorIcon from './outdoor';
+import PersonalFinanceIcon from './personal-finance';
+import PetIcon from './pet';
+import PlantIcon from './plant';
+import SelfDevIcon from './self-dev';
+import ShowIcon from './show';
+import SocialIcon from './social';
+import SportsIcon from './sports';
+import SportsWatchIcon from './sports-watch';
+import VolunteerIcon from './volunteer';
+
+export {
+  BeautyIcon,
+  BookIcon,
+  CareerIcon,
+  CarIcon,
+  CraftIcon,
+  CultureIcon,
+  DanceIcon,
+  DrinkIcon,
+  EmploymentIcon,
+  EnvironmentIcon,
+  FamilyIcon,
+  FoodIcon,
+  ForeignLanguageIcon,
+  GameIcon,
+  HealthIcon,
+  InteriorIcon,
+  LoveIcon,
+  MediaIcon,
+  MindIcon,
+  MusicIcon,
+  OutdoorIcon,
+  PersonalFinanceIcon,
+  PetIcon,
+  PlantIcon,
+  SelfDevIcon,
+  ShowIcon,
+  SocialIcon,
+  SportsIcon,
+  SportsWatchIcon,
+  VolunteerIcon,
+};
