1e237aec41c493d8a363a693420859a900c141b3
ring-wdr <akswnd55@gmail.com>
2025-01-24 23:05:24 +0900

    chore: forwardref remove

--- ui/icon/clock.tsx
+++ ui/icon/clock.tsx
@@ -1,11 +1,12 @@
-import { forwardRef, SVGProps } from 'react';
+import { SVGProps } from 'react';
 
-const ClockIcon = forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>((props, ref) => (
-  <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" ref={ref} {...props}>
-    <circle cx="9" cy="9" r="8" fill="#D9D9D9" stroke="currentColor" strokeWidth="2" />
-    <path d="M9 6V9.85714H12.4286" stroke="currentColor" strokeWidth="2" />
-  </svg>
-));
-ClockIcon.displayName = 'ClockIcon';
+function ClockIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <circle cx="9" cy="9" r="8" fill="#D9D9D9" stroke="currentColor" strokeWidth="2" />
+      <path d="M9 6V9.85714H12.4286" stroke="currentColor" strokeWidth="2" />
+    </svg>
+  );
+}
 
 export default ClockIcon;
