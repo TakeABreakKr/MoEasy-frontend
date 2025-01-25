fad89d149f1eae62dc6bde3fdf7f00a5bcc7439b
ring-wdr <akswnd55@gmail.com>
2025-01-25 23:59:06 +0900

    Refactor: 하드코딩 제거

--- packages/service/__test__/handlers/meeting.ts
+++ packages/service/__test__/handlers/meeting.ts
@@ -3,10 +3,14 @@ import { http, HttpResponse } from 'msw';
 import { MeetingType } from '@/entities/meeting/api';
 import { initializeMeeting } from '@/entities/meeting/api/mock';
 
+const MOCK_API_BASE = 'http://localhost:4000/mock';
+const MEETING_ENDPOINT = `${MOCK_API_BASE}/meeting/:meetingId`;
+
 export const meetingHandlers = [
-  http.get('http://localhost:4000/mock/meeting/:meetingId', ({ params }) => {
+  http.get(MEETING_ENDPOINT, ({ params }) => {
     if (params.meetingId) {
       return HttpResponse.json<MeetingType>(initializeMeeting(Number(params.meetingId)));
     }
+    return new HttpResponse(null, { status: 400, statusText: '잘못된 요청입니다.' });
   }),
 ];
