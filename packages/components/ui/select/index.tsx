9ce8104027c614d5ab7bb79cbb9276c2ef6bbea9
ring-wdr <akswnd55@gmail.com>
2025-02-03 22:22:01 +0900

    fix: 손상된 yaml + popover의 react 19 대응

--- packages/components/ui/select/index.tsx
+++ packages/components/ui/select/index.tsx
@@ -12,21 +12,21 @@ const PopoverTrigger = PopoverPrimitive.Trigger;
 
 const PopoverAnchor = PopoverPrimitive.Anchor;
 
-const PopoverContent = React.forwardRef<
-  React.ElementRef<typeof PopoverPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
->(({ className, align = 'center', sideOffset = 4, ...props }, ref) => (
+const PopoverContent = ({
+  className,
+  align = 'center',
+  sideOffset = 4,
+  ...props
+}: React.ComponentPropsWithRef<typeof PopoverPrimitive.Content>) => (
   <PopoverPrimitive.Portal>
     <PopoverPrimitive.Content
-      ref={ref}
       align={align}
       sideOffset={sideOffset}
       className={clsx(dropdownMenuContent, className)}
       {...props}
     />
   </PopoverPrimitive.Portal>
-));
-PopoverContent.displayName = PopoverPrimitive.Content.displayName;
+);
 
 const PopoverClose = PopoverPrimitive.Close;
 
