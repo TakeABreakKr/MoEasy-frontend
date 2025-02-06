c91117e1d210e6c911971df4c45a83b4a8edc29d
ring-wdr <akswnd55@gmail.com>
2025-02-07 00:53:22 +0900

    design: about 페이지 푸터 등록

--- ui/footer/footer.css.ts
+++ ui/footer/footer.css.ts
@@ -3,12 +3,15 @@ import { style } from '@vanilla-extract/css';
 
 export const footer = style({
   display: 'flex',
-  alignItems: 'center',
-  maxWidth: rem(80 * 16),
-  padding: rem(60),
+  width: '100%',
+  justifyContent: 'center',
+  scrollSnapAlign: 'end' /* 스크롤 위치 맞춤 */,
 });
 
 export const footerContent = style({
   display: 'flex',
   flexDirection: 'column',
+  padding: rem(60),
+  width: '100%',
+  maxWidth: rem(80 * 16),
 });
