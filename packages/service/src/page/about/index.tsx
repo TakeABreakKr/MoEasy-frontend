0a8269c926e25f1854cd003cb1c2b2f811e7d928
ring-wdr <akswnd55@gmail.com>
2025-02-07 00:53:22 +0900

    design: about 페이지 푸터 등록

--- packages/service/src/page/about/index.tsx
+++ packages/service/src/page/about/index.tsx
@@ -6,15 +6,19 @@ import clsx from 'clsx';
 import { AboutToNextPage } from '@/widget/about/to-next-page';
 
 import { suite } from '@moeasy/storybook/font';
+import { MainFooter } from '@moeasy/storybook/ui/footer';
 
 import * as styles from './about.css';
 
 export function AboutPage({ children }: { children?: React.ReactNode }) {
   const parentRef = useRef<HTMLElement>(null);
   return (
-    <main ref={parentRef} className={clsx(styles.aboutMain, suite.className)}>
-      {children}
-      <AboutToNextPage parentRef={parentRef} />
-    </main>
+    <>
+      <main ref={parentRef} className={clsx(styles.aboutMain, suite.className)}>
+        {children}
+        <AboutToNextPage parentRef={parentRef} />
+        <MainFooter />
+      </main>
+    </>
   );
 }
