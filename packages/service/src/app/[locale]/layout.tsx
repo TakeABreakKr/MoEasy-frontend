f2b7b1c1af90721070b8c057a7fef9e183d02f75
ring-wdr <akswnd55@gmail.com>
2025-02-14 01:33:46 +0900

    design: 로그인 시 헤더 디자인 변경

--- packages/service/src/app/[locale]/layout.tsx
+++ packages/service/src/app/[locale]/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { cookies } from 'next/headers';
 import clsx from 'clsx';
 
 import { LoginPopup } from '@/widget/main/popup/login';
@@ -25,11 +26,13 @@ export default async function RootLayout({
   params: Promise<{ locale: string }>;
 }>) {
   const { locale } = await params;
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get('AccessToken')?.value;
 
   return (
     <html lang={locale}>
       <body className={clsx(pretendard.className, pretendard.variable)}>
-        <Header />
+        <Header isLogin={!!accessToken} />
         <MSWWrapper />
         <RootProvider locale={locale}>
           <LoginPopup />
