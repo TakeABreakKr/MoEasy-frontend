589af86028064cb6c7238da45139dd077e5825cd
ring-wdr <akswnd55@gmail.com>
2025-02-14 01:15:41 +0900

    design: 활동카드 활동명 ellipsis 주기

--- ui/text/index.tsx
+++ ui/text/index.tsx
@@ -1,25 +1,20 @@
-import { ComponentPropsWithoutRef, forwardRef } from 'react';
+import { ComponentPropsWithRef } from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { RecipeVariants } from '@vanilla-extract/recipes';
 import clsx from 'clsx';
 
 import { textRecipe } from './text.css';
 
-export type TextProps = { asChild?: boolean } & RecipeVariants<typeof textRecipe> & ComponentPropsWithoutRef<'div'>;
+export type TextProps = { asChild?: boolean } & RecipeVariants<typeof textRecipe> & ComponentPropsWithRef<'div'>;
 
-const Text = forwardRef<HTMLDivElement, TextProps>(function (
-  { asChild, className, body, display, headline, title, label, semibold, ...props },
-  ref,
-) {
+function Text({ asChild, className, body, display, headline, title, label, semibold, ellipsis, ...props }: TextProps) {
   const Comp = asChild ? Slot : 'div';
   return (
     <Comp
-      ref={ref}
-      className={clsx(textRecipe({ body, display, headline, title, label, semibold }), className)}
+      className={clsx(textRecipe({ body, display, headline, title, label, semibold, ellipsis }), className)}
       {...props}
     />
   );
-});
-Text.displayName = 'Text';
+}
 
 export { Text };
