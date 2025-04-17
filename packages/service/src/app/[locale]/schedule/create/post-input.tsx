c51447a4677442da91787a4269f3e619ce0f00ca
ring-wdr <akswnd55@gmail.com>
2025-04-17 22:39:39 +0900

    feat: profile page initial

--- packages/service/src/app/[locale]/schedule/create/post-input.tsx
+++ packages/service/src/app/[locale]/schedule/create/post-input.tsx
@@ -1,23 +1,27 @@
 'use client';
-import { useState } from 'react';
+
+import { Address } from '@/shared/type/daum-post';
 
 import { SearchButton } from '@moeasy/storybook/ui/button';
 
-export default function PostInput() {
-  const [address, setAddress] = useState('');
+export default function PostInput({
+  address,
+  setAddress,
+}: {
+  address?: Address;
+  setAddress?: (address: Address) => void;
+}) {
   return (
     <SearchButton
       style={{ width: '100%' }}
       onClick={() => {
         if (window.daum)
           new window.daum.Postcode({
-            oncomplete: (data) => {
-              setAddress(`${data.zonecode} ${data.address}`);
-            },
+            oncomplete: (data) => setAddress?.(data),
           }).open();
       }}
     >
-      {address}
+      {address?.sigungu}
     </SearchButton>
   );
 }
